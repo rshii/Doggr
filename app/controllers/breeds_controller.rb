cba1243f88e70ac73b76d4a911059155c4be864b
Rishi Sharma <contact@rishisharma.io>
2018-08-28 23:46:18 +0000

    Added:sub_breed counter;changed breed seed route;API calls for images;

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,21 +1,22 @@
 class BreedsController < ApplicationController
     def index
-        
+        @breeds = Breed.all
     end
-    def create
-        redirect_to action: "index"
-        response = HTTParty.get('https://dog.ceo/api/breeds/list/all')
-        breeds = JSON.parse(response.body)["message"]
-        breeds.each do |breed|
-            breed_data = Breed.new(breed_name: breed[0])
-            #Build sub_breeds
-            if !breed[1].empty?
-                breed[1].each do |sub_breed|
-                    breed_data.sub_breeds.build(sub_breed_name:sub_breed)
+    def seed
+        if Breed.first.nil?
+            response = HTTParty.get('https://dog.ceo/api/breeds/list/all')
+            breeds = JSON.parse(response.body)["message"]
+            breeds.each do |breed|
+                breed_data = Breed.new(breed_name: breed[0])
+                if !breed[1].empty?
+                    breed[1].each do |sub_breed|
+                        breed_data.sub_breeds.build(sub_breed_name:sub_breed)
+                    end
                 end
-            breed_data.save
+                breed_data.save
             end
         end
+        redirect_to breeds_url
     end
     def show
     end
